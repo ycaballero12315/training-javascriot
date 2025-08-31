265fd5f2d580f007f1122f90b41f7de95cc2e43c
= <=>
2025-08-31 00:59:16 -0300

    Actualizacion de test automatico

--- test_node/cypress/e2e/mytest.cy.js
+++ test_node/cypress/e2e/mytest.cy.js
@@ -1,5 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://www.mercadolibre.com.ar')
+    cy.get('[name="as_word"]').type('Alquileres{enter}')
   })
 })
